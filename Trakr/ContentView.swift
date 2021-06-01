f2d5bdc7c9d7746c751852d73a5b6b0bdfb4e17e
Playground <christopher@playground.tv>
2021-06-01 04:14:03 +0200

    Initial Commit

---
+++ Trakr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trakr
+//
+//  Created by Christopher on 2021-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trakr/TrakrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrakrApp.swift
+//  Trakr
+//
+//  Created by Christopher on 2021-06-01.
+//
+
+import SwiftUI
+
+@main
+struct TrakrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
